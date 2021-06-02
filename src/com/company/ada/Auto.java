637dcf8e4d0ad2c72b4e69d7c408e650ba31edad
jesibelen <jesica.salva2@gmail.com>
2021-06-02 17:31:17 -0300

    lo arreglé

--- src/com/company/ada/Auto.java
+++ src/com/company/ada/Auto.java
@@ -1,50 +1,22 @@
 package com.company.ada;
 
 public class Auto {
-    MotorFord motor;
-    MotorPeugeot motorP;
-    MotorAudi motorA;
-    String patente;
-    Integer cantidad_puertas;
+    private Motor motor;
+    private String patente;
+    private Integer cantidad_puertas;
 
-    public Auto(MotorFord motor, String patente, Integer cantidad_puertas) {
+    public Auto(Motor motor, String patente, Integer cantidad_puertas) {
         this.motor = motor;
         this.patente = patente;
         this.cantidad_puertas = cantidad_puertas;
     }
-    public Auto(MotorAudi motorA, String patente, Integer cantidad_puertas) {
-        this.motorA = motorA;
-        this.patente = patente;
-        this.cantidad_puertas = cantidad_puertas;
-    }
-    public Auto(MotorPeugeot motorP, String patente, Integer cantidad_puertas) {
-        this.motorP = motorP;
-        this.patente = patente;
-        this.cantidad_puertas = cantidad_puertas;
-    }
 
-    public MotorFord getMotor() {
+    public Motor getMotor() {
         return motor;
     }
 
     public void setMotor(MotorFord motor) {
         this.motor = motor;
-    }
-
-    public MotorPeugeot getMotorP() {
-        return motorP;
-    }
-
-    public void setMotorP(MotorPeugeot motorP) {
-        this.motorP = motorP;
-    }
-
-    public MotorAudi getMotorA() {
-        return motorA;
-    }
-
-    public void setMotorA(MotorAudi motorA) {
-        this.motorA = motorA;
     }
 
     public String getPatente() {
@@ -67,10 +39,16 @@ public class Auto {
     public String toString() {
         return "Auto{" +
                 "motor=" + motor +
-                ", motorP=" + motorP +
-                ", motorA=" + motorA +
                 ", patente='" + patente + '\'' +
                 ", cantidad_puertas=" + cantidad_puertas +
                 '}';
+    }
+
+    public void hacerEncender(Motor motor) {
+        motor.encender();
+    }
+
+    public void acelera(Motor motor) {
+        motor.acelerar();
     }
 }
--- src/com/company/ada/Main.java
+++ src/com/company/ada/Main.java
@@ -4,17 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
         Auto miAuto1 = new Auto(new MotorAudi(),"AAA123",4);
         Auto miAuto2 = new Auto(new MotorFord(),"ABV234",2);
         Auto miAuto3 = new Auto(new MotorPeugeot(),"CDE542",4);
 
-        miAuto1.getMotorA().encender();
-        miAuto1.getMotorA().acelerar();
-
-        miAuto2.getMotor().encender();
-        miAuto3.getMotorP().encender();
-
+        miAuto1.hacerEncender(miAuto1.getMotor());
+        miAuto1.acelera(miAuto1.getMotor());
+        miAuto2.hacerEncender(miAuto2.getMotor());
 
         System.out.println(miAuto1+"\n"+miAuto2+"\n"+miAuto3);
     }
